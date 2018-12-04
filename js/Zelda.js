cdb5f08df58d9404aefc564280fdf7925aabc6d1
Atesaki4 <42302962+Atesaki4@users.noreply.github.com>
2018-12-03 23:45:56 -0200

    patch 0

---
+++ js/Zelda.js
@@ -0,0 +1,12 @@
+function cityOnClick(){
+	window.location.href("./Zelda.html#descricao");
+}
+
+
+/*----------------------------------*/
+
+$.ready(function(){
+
+	$("city1.1").onClick(cityOnClick);
+
+});
